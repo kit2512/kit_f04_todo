99a891aca9a54037b916bfeb9cbec3f0f779c377
nguyendangvu221 <nguyendangvu221@gmail.com>
2022-09-05 21:19:04 +0700

    task

---
+++ lib/src/data/task.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class Task{
+  String? name;
+  Color? color;
+  DateTime? time;
+  String? place;
+  String? level;
+  Task(this.name, this.color, this.time, this.place, this.level);
+}
---
+++ lib/src/data/task_manager.dart
@@ -0,0 +1,20 @@
+import 'task.dart';
+
+class TaskManager {
+  List<Task> tasks = [];
+  void addTask(Task task) {
+    tasks.add(task);
+  }
+
+  void removeTask(Task task) {
+    tasks.remove(task);
+  }
+
+  void updateTask(Task task) {
+    tasks[tasks.indexOf(task)] = task;
+  }
+
+  List<Task> getTasks() {
+    return tasks;
+  }
+}
