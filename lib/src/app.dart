757d203eef878f1d85486a68def7e0a24dfc20c3
TrinhLinh201 <97020160+TrinhLinh201@users.noreply.github.com>
2022-09-07 01:12:47 +0700

    Update app.dart

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,13 +1,6 @@
-import 'package:demo_state_app/src/ui/profile_body/profile.dart';
-import 'package:demo_state_app/src/ui/task_config/task_color.dart';
 import 'package:demo_state_app/src/ui/task_config/task_config.dart';
 
-import 'package:demo_state_app/src/ui/task_config/task_name.dart';
-import 'package:demo_state_app/src/ui/task_config/task_place.dart';
 import 'package:flutter/material.dart';
-
-import 'ui/home_body/todo_screen.dart';
-import 'ui/task_config/task_time.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
