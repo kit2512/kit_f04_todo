7868b0aebb0125c28d3f0bafcbfdac2901b0b0cb
nguyendangvu221 <nguyendangvu221@gmail.com>
2022-09-07 13:53:37 +0700

    update_task

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,5 @@
 import 'package:demo_state_app/src/ui/profile_body/profile.dart';
 import 'package:demo_state_app/src/ui/screen/home_screen.dart';
-import 'package:demo_state_app/src/ui/screen/task_screen.dart';
 import 'package:demo_state_app/src/ui/task_config/task_color.dart';
 import 'package:demo_state_app/src/ui/task_config/task_level.dart';
 import 'package:demo_state_app/src/ui/task_config/task_name.dart';
@@ -8,6 +7,7 @@ import 'package:demo_state_app/src/ui/task_config/task_place.dart';
 import 'package:flutter/material.dart';
 
 import 'ui/home_body/todo_screen.dart';
+import 'ui/screen/task_screen.dart';
 import 'ui/task_config/task_time.dart';
 
 class MyApp extends StatelessWidget {
