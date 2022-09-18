c10ff73c264e1c20e2de6efcb7b7d309c426b797
TrinhLinh201 <97020160+TrinhLinh201@users.noreply.github.com>
2022-09-18 14:16:55 +0700

    Update task_screen.dart

--- lib/src/ui/screen/task_screen.dart
+++ lib/src/ui/screen/task_screen.dart
@@ -56,7 +56,7 @@ class TaskConfig extends StatelessWidget {
           margin: const EdgeInsets.only(left: 10, right: 10),
           child: Column(
             children: [
-              TaskName(name: task2.name),
+              TaskName(name: task2.name!),
               const TaskColor(),
               TaskDate(
                 dateTime: task2.date!,
@@ -75,7 +75,7 @@ class TaskConfig extends StatelessWidget {
 
   void saveData(BuildContext context) {
     Task task2 = context.read<TaskConfigManager>().task;
-    log(task2.name + " con lom koh");
+
     if (titleAppBar == "Add Task") {
       context.read<TaskManager>().addTask(task2);
     } else {
