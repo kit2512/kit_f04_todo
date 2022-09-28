804b95d854717c6f867fc661b2d12f65e380a256
Trịnh Linh <trinhhoailinh201@gmail.com>
2022-09-28 11:01:04 +0700

    Update list_color.dart

--- lib/src/provider/list_color.dart
+++ lib/src/provider/list_color.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 
 class ColorList {
   List<Color> colorLists = const [
-    Color.fromARGB(255, 123, 174, 216),
+    Color.fromARGB(255, 239, 236, 130),
     Color.fromARGB(255, 232, 226, 176),
     Color.fromARGB(255, 22, 211, 236),
-    Color.fromARGB(255, 159, 187, 127),
-    Color.fromARGB(255, 173, 129, 129),
-    Color.fromARGB(255, 231, 127, 162),
-    Color.fromARGB(255, 47, 187, 124),
+    Color.fromARGB(255, 195, 239, 144),
+    Color.fromARGB(255, 240, 176, 176),
+    Color.fromARGB(255, 241, 166, 191),
+    Color.fromARGB(255, 68, 232, 158),
     Color.fromARGB(255, 105, 214, 233),
-    Color.fromARGB(255, 205, 243, 101),
-    Color.fromARGB(255, 242, 137, 240),
-    Color.fromARGB(255, 108, 255, 149),
+    Color.fromARGB(255, 196, 234, 92),
+    Color.fromARGB(255, 247, 144, 245),
+    Color.fromARGB(255, 131, 237, 161),
   ];
 }
